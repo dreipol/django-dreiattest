487943a8f18ba8d011ae7740ee3be9b6850ab503
Sidney <hello@sidney.dev>
2021-09-14 17:02:39 +0200

    Fix middleware

--- dreiattest/middlewares.py
+++ dreiattest/middlewares.py
@@ -1,5 +1,3 @@
-from types import Union
-
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import JsonResponse
 from pyattest.exceptions import PyAttestException
@@ -8,12 +6,19 @@ from dreiattest.exceptions import DreiAttestException
 
 
 class HandleDreiattestExceptionsMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        return self.get_response(request)
+
     def process_exception(self, request: WSGIRequest, exception: Exception):
         if isinstance(exception, (PyAttestException, DreiAttestException)):
             return self.handle(request, exception)
 
     def handle(self, request: WSGIRequest, exception: Exception):
-        name = exception.__class__.__name__
-        code = name.removesuffix('Exception')
+        code = exception.__class__.__name__
+        if code.endswith('Exception'):
+            code = code[:-9]
 
         return JsonResponse(data={'code': code}, status=400)
